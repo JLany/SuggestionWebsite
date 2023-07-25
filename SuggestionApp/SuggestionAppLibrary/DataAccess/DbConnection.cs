33b1c709329411af6497578e489aa979afd27111
Kilany <y.m.elkilany@gmail.com>
2023-07-25 22:28:18 +0300

    Create DbConnection

---
+++ SuggestionApp/SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class DbConnection : IDbConnection
+{
+    private readonly IConfiguration _config;
+    private readonly IMongoDatabase _database;
+    private const string ConnectionName = "MongoDB";
+
+    public string DbName { get; private set; }
+
+    public string CategoryCollectionName { get; } = "categories";
+    public string StatusCollectionName { get; } = "statuses";
+    public string UserCollectionName { get; } = "users";
+    public string PostCollectionName { get; } = "posts";
+
+    public IMongoClient Client { get; private set; }
+    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+    public IMongoCollection<StatusModel> StatusCollection { get; private set; }
+    public IMongoCollection<UserModel> UserCollection { get; private set; }
+    public IMongoCollection<PostModel> PostCollection { get; private set; }
+
+    public DbConnection(IConfiguration config)
+    {
+        _config = config;
+
+        Client = new MongoClient(_config.GetConnectionString(ConnectionName));
+
+        DbName = _config["DatabaseName"];
+        _database = Client.GetDatabase(DbName);
+
+        CategoryCollection = _database.GetCollection<CategoryModel>(CategoryCollectionName);
+        StatusCollection = _database.GetCollection<StatusModel>(StatusCollectionName);
+        UserCollection = _database.GetCollection<UserModel>(UserCollectionName);
+        PostCollection = _database.GetCollection<PostModel>(PostCollectionName);
+    }
+}
---
+++ SuggestionApp/SuggestionAppLibrary/DataAccess/IDbConnection.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+
+namespace SuggestionAppLibrary.DataAccess
+{
+    public interface IDbConnection
+    {
+        IMongoCollection<CategoryModel> CategoryCollection { get; }
+        IMongoClient Client { get; }
+        string DbName { get; }
+        IMongoCollection<PostModel> PostCollection { get; }
+        IMongoCollection<StatusModel> StatusCollection { get; }
+        IMongoCollection<UserModel> UserCollection { get; }
+    }
+}
