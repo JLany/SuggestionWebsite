65b0f6e8f6512738623ff87f8b287997c45107da
Kilany <y.m.elkilany@gmail.com>
2023-08-02 16:34:05 +0300

    Add Create page

---
+++ SuggestionApp/SuggestionAppUI/Models/CreatePostDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionAppUI.Models;
+
+public class CreatePostDto
+{ 
+	[Required]
+	[MaxLength(75)]
+	public string Title { get; set; }
+
+	[Required]
+	[MinLength(1)]
+	[Display (Name = "Category")]
+	public string CategoryId { get; set; }
+
+	[MaxLength(500)]
+	public string Description { get; set; }
+}
